0d1213ad2de3a5d5b8b513651f050353da7806e9
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2026-06-11 18:13:11 +0100

    Remove calendar delete tool

--- internal/twprojects/tools.go
+++ internal/twprojects/tools.go
@@ -187,7 +187,6 @@ func DefaultToolsetGroup(readOnly, allowDelete bool, engine *twapi.Engine) *tool
 	}
 	if allowDelete {
 		timeWriteTools = append(timeWriteTools,
-			CalendarDelete(engine),
 			TimelogDelete(engine),
 			TimerDelete(engine),
 		)
